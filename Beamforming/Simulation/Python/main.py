933b2f35344f9972c87219cc3880ebc0a7ac8104
paycheckpacheck <pacheck@vt.edu>
2025-03-01 21:27:51 -0500

    Added Primitive beamforming scripts to beamforming/simulation/matlab

---
+++ Beamforming/Simulation/Python/main.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+class Antenna:
+    def __init__(self, coords, freq=2.5e9):
+        self.coords = coords
+
+    pass
+
+class AntennaArray:
+
+    pass
+
+
+class Beamformer:
+
+
+    def compute_steeringVector(self, azim, elev):
+        
+        pass
+
+    pass
+beam = Beamformer()
+beam.compute_steeringVector()
